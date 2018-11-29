030bbcbe927b7f7c7885fd8fb6130209cc133173
johnthedesigner <john@johnthedesigner.com>
2018-11-28 20:15:28 -0500

    Added basic router with query params links

---
+++ src/Colors.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Colors extends Component {
+  render() {
+    const styles = {
+      width: "20px",
+      height: "20px",
+      backgroundColor: "red"
+    };
+    return <div style={styles} />;
+  }
+}
+
+export default Colors;
